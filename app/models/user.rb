cc3838ad6d458620727f4be72978f821b7a3a3d7
Hivna <hivna12@gmail.com>
2024-12-27 12:29:53 -0300

    feat: set up database, generated migrations, and configured models

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  has_secure_password
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :nullify
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, presence: true, length: { minimum: 8 }, confirmation: true
+end
